ddc6c9fc4d9c55063c748daae47c394579ab7805
Sasamus <labus93@gmail.com>
2015-02-03 17:49:37 +0100

    Implemented the base of the Tester class

--- src/ae/logic/testing/Tester.java
+++ src/ae/logic/testing/Tester.java
@@ -1,5 +1,9 @@
 package ae.logic.testing;
 
+import se.miun.dt015a.logic.LogicExpressions;
+import se.miun.dt015a.logic.NaivLogicExpressions;
+import se.miun.dt015a.logic.SimplifiedExpressions;
+
 /**
  * A class with a main method for testing
  * 
@@ -11,7 +15,15 @@ public class Tester {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+
+		LogicExpressions naivLogicExpressions = new NaivLogicExpressions();
+
+		LogicExpressions simplifiedExpressions = new SimplifiedExpressions();
+
+		boolean A1 = true;
+		boolean A2 = true;
+
+		System.out.println(naivLogicExpressions.expression1(A1) == simplifiedExpressions.expression1(A2));
 
 	}
 
