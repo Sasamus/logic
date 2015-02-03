3d2dd399eff7f4f305312a4d32bc9262d10bf9df
Sasamus <labus93@gmail.com>
2015-02-03 17:37:40 +0100

    Created a Tester class

---
+++ src/ae/logic/testing/Tester.java
@@ -0,0 +1,18 @@
+package ae.logic.testing;
+
+/**
+ * A class with a main method for testing
+ * 
+ * @author Albin Engström
+ */
+public class Tester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
